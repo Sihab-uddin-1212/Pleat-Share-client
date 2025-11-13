de9f600465a6c0e3c23222737a1ddf9a99fde260
Sihab-uddin-1212 <shihabsk07742@gmail.com>
2025-11-13 22:01:50 +0600

    DatePicker

--- src/pages/AddFood.jsx
+++ src/pages/AddFood.jsx
@@ -1,133 +1,136 @@
-import React, { use } from 'react';
-import { toast } from 'react-toastify';
-import { AuthContext } from '../Auth/AuthContext';
+import React, { use, useState } from "react";
+import { toast } from "react-toastify";
+import { AuthContext } from "../Auth/AuthContext";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddFood = () => {
+  const [expireDate, setExpireDate] = useState(new Date());
+  console.log(expireDate)
 
-  const { user } = use(AuthContext)
+  const { user } = use(AuthContext);
+
   // console.log(user)
 
-
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const formData = {
       food_name: e.target.name.value,
       pickup_location: e.target.location.value,
-      food_quantity:e.target.quantity.value,
+      food_quantity: e.target.quantity.value,
       food_image: e.target.food_img.value,
-      expire_date:e.target.date.value,
-      status:'available',
+      expire_date:expireDate,
+      status: "available",
       email: user.email,
-      donator_name:user.displayName,
-      donator_image:user.photoURL
-    }
-    // console.log(formData)
+      donator_name: user.displayName,
+      donator_image: user.photoURL,
+    };
+    console.log(formData)
 
-    fetch('https://plate-share-server-ten.vercel.app/foods', {
+    fetch("https://plate-share-server-ten.vercel.app/foods", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData)
+      body: JSON.stringify(formData),
     })
-    .then(res => res.json())
-    .then(data=> {
-      toast.success("Successfully added!")
-      // console.log(data)
-    })
-    .catch(err => {
-      // console.log(err)
-    })
-   
+      .then((res) => res.json())
+      .then((data) => {
+        toast.success("Successfully added!");
+        // console.log(data)
+      })
+      .catch((err) => {
+        // console.log(err)
+      });
+  };
 
-  }
+  return (
+    <div>
+      <div className="card border border-gray-200 bg-base-100 w-full max-w-md mx-auto shadow-2xl rounded-2xl">
+        <div className="card-body p-6 relative">
+          <h2 className="text-2xl font-bold text-center mb-6">Add New Model</h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label className="label font-medium">Food Name</label>
+              <input
+                type="text"
+                name="name"
+                required
+                className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
+                placeholder="Food name"
+              />
+            </div>
 
- 
+            {/* Location*/}
+            <div>
+              <label className="label font-medium">Location</label>
 
-    return (
-        <div>
-             <div className="card border border-gray-200 bg-base-100 w-full max-w-md mx-auto shadow-2xl rounded-2xl">
-      <div className="card-body p-6 relative">
-        <h2 className="text-2xl font-bold text-center mb-6">Add New Model</h2>
-        <form 
-        onSubmit={handleSubmit}
-         className="space-y-4">
-         
-          <div>
-            <label className="label font-medium">Food Name</label>
-            <input
-              type="text"
-              name="name"
-              required
-              className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
-              placeholder="Food name"
-            />
-          </div>
+              <input
+                type="text"
+                name="location"
+                required
+                className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
+                placeholder="Location"
+              />
+            </div>
+            <div>
+              <label className="label font-medium">Quantity</label>
 
-          {/* Location*/}
-          <div>
-            <label className="label font-medium">Location</label>
-             
-                <input
-              type="text"
-              name="location"
-              required
-              className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
-              placeholder="Location"
-            />
-              
-          </div>
-          <div>
-            <label className="label font-medium">Quantity</label>
-             
-                <input
-              type="number"
-              min="1"
-              name="quantity"
-              required
-              className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
-              placeholder="Food Quantity"
-            />
-              
-          </div>
+              <input
+                type="number"
+                min="1"
+                name="quantity"
+                required
+                className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
+                placeholder="Food Quantity"
+              />
+            </div>
 
-          {/* Description Textarea */}
-          <div>
-            <label className="label font-medium">Expire date</label>
-            <textarea
+            {/* Description Textarea */}
+            <div>
+              <label className="label font-medium">Expire date</label>
+              <DatePicker
+                selected={expireDate}
+                onChange={(date) => setExpireDate(date)}
+                dateFormat="yyyy-MM-dd"
+                minDate={new Date()}
+                className="input w-full rounded-full bg-white/10   placeholder-gray-300 focus:outline-none border border-gray-400/30 px-4 py-2 cursor-pointer"
+                placeholderText="Select expire date"
+              />
+              {/* <textarea
               name="date"
               required
               
              className="textarea w-full rounded-2xl focus:border-0 focus:outline-gray-200  "
               placeholder="expire_date"
-            ></textarea>
-          </div>
+            ></textarea> */}
+            </div>
 
-          {/* Thumbnail URL */}
-          <div>
-            <label className="label font-medium">Food Url</label>
-            <input
-              type="url"
-              name="food_img"
-              required
-              className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
-              placeholder="https://example.com/image.jpg"
-            />
-          </div>
+            {/* Thumbnail URL */}
+            <div>
+              <label className="label font-medium">Food Url</label>
+              <input
+                type="url"
+                name="food_img"
+                required
+                className="input w-full rounded-full focus:border-0 focus:outline-gray-200"
+                placeholder="https://example.com/image.jpg"
+              />
+            </div>
 
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="btn w-full text-white mt-6 rounded-full bg-linear-to-r from-pink-500 to-red-600 hover:from-pink-600 hover:to-red-700"
-          >
-            Add Food 
-          </button>
-        </form>
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="btn w-full text-white mt-6 rounded-full bg-linear-to-r from-pink-500 to-red-600 hover:from-pink-600 hover:to-red-700"
+            >
+              Add Food
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-        </div>
-    );
+  );
 };
 
 export default AddFood;
--- src/pages/AllFood.jsx
+++ src/pages/AllFood.jsx
@@ -1,57 +1,51 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import FoodDetails from '../component/FoodCard';
-import FoodCard from '../component/FoodCard';
-import Loader from './Loader';
- 
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import FoodDetails from "../component/FoodCard";
+import FoodCard from "../component/FoodCard";
+import Loader from "./Loader";
 
 const AllFood = () => {
-    const {loading,setLoading} = useState(true)
+  const { loading, setLoading } = useState(true);
 
-    const [foods,setFoods] = useState()
-    // console.log(foods)
+  const [foods, setFoods] = useState();
+  // console.log(foods)
 
-    useEffect(()=>{
-        
-        axios(`https://plate-share-server-ten.vercel.app/foods?status=${"available"}`)
-        .then(data => {
-             
-            const shortData = data?.data.sort((a,b)=>b.food_quantity-a.food_quantity)
-            setFoods(shortData)
-            // console.log(shortData)
-        })
-    },[])
+  useEffect(() => {
+    axios(
+      `https://plate-share-server-ten.vercel.app/foods?status=${"available"}`
+    ).then((data) => {
+      const shortData = data?.data.sort(
+        (a, b) => b.food_quantity - a.food_quantity
+      );
+      setFoods(shortData);
+      // console.log(shortData)
+    });
+  }, []);
 
-    
-
-    return (
+  return (
+    <div>
+      {loading ? (
+        <Loader></Loader>
+      ) : (
         <div>
-            {loading
-            ?
-            <Loader></Loader>
-             :
-             <div>
-                <div className="relative flex flex-col items-center my-16 text-center">
-  <span className="uppercase tracking-widest text-sm text-amber-400 font-semibold mb-2">
-    Available Now
-  </span>
-  <h2 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 via-amber-200 to-purple-300 drop-shadow-md">
-    Available <span className="text-amber-300">Foods</span>
-  </h2>
-  <div className="mt-3 w-24 h-1 rounded-full bg-gradient-to-r from-amber-400 via-pink-400 to-purple-400 opacity-70"></div>
-</div>
-     <div className="grid grid-cols-3 lg:grid-cols-4 gap-3 mt-10">
-        {foods?.map((food) => (
-            <FoodCard  key={food._id} food={food}></FoodCard>
-         ))}
-      </div>
-             </div>
-            
-      }
-        
-           
+          <div className="relative flex flex-col items-center my-16 text-center">
+            <span className="uppercase tracking-widest text-sm text-amber-400 font-semibold mb-2">
+              Available Now
+            </span>
+            <h2 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 via-amber-200 to-purple-300 drop-shadow-md">
+              Available <span className="text-amber-300">Foods</span>
+            </h2>
+            <div className="mt-3 w-24 h-1 rounded-full bg-gradient-to-r from-amber-400 via-pink-400 to-purple-400 opacity-70"></div>
+          </div>
+          <div className="grid grid-cols-3 lg:grid-cols-4 gap-3 mt-10">
+            {foods?.map((food) => (
+              <FoodCard key={food._id} food={food}></FoodCard>
+            ))}
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default AllFood;
